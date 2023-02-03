814f2120652a27b7b73df25fb39d00446d23e736
ra1lmuz <2003muzafarov@mail.ru>
2023-02-03 15:04:05 +0300

    Add: component

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import router from "./router/router";
+import { RouterProvider } from "react-router-dom";
+
+import "./styles/index.scss"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.Suspense fallback={<h1>Loading...</h1>}>
+    <RouterProvider router={router} />
+  </React.Suspense>
+);
